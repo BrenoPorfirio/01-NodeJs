b0eed7305bcbf75038482923caccd44cc4923ffb
Breno Porfirio <brenoporfirio03@gmail.com>
2024-03-20 22:31:24 -0300

    feat: HTTP status codes

--- src/server.js
+++ src/server.js
@@ -24,10 +24,10 @@ const server = http.createServer((req, res) => {
             email: 'johndoe@gmail.com',
         })
 
-        return res.end('Criação de usuários')
+        return res.writeHead(201).end()
     }
 
-    return res.end('Hello World!');
+    return res.writeHead(404).end('Not Found');
 })
 
 server.listen(3333)
