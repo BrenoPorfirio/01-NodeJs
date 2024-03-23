0848818b5c67c13d168c4eb1945a80867dda93ff
Breno Porfirio <brenoporfirio03@gmail.com>
2024-03-22 21:12:36 -0300

    feat: applying streams in the http module

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -29,7 +29,7 @@ class MultiplyByTenStream extends Writable {
     _write(chunk, encoding, callback) {
         console.log(Number(chunk.toString()) * 10)
         callback()
-    }                                       
+    }
 }
 
 new OneToHundredStream()
