e18020a51394f4ff2ec31968e00104821fb71a8d
Breno Porfirio <brenoporfirio03@gmail.com>
2024-03-24 18:03:01 -0300

    feat: creating regex from parameters

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { Database } from './database.js'
 import { randomUUID } from 'node:crypto'
+import { builRoutePath } from './utils/build-route-path.js'
 
 const database = new Database()
 
 export const routes = [
     {
         method: 'GET',
-        path: '/users',
+        path: builRoutePath('/users'),
         handler: (req, res) => {
             const users = database.select('users')
 
@@ -15,7 +16,7 @@ export const routes = [
     },
     {
         method: 'POST',
-        path: '/users',
+        path: builRoutePath('/users'),
         handler: (req, res) => {
             const { name, email } = req.body
 
@@ -32,7 +33,7 @@ export const routes = [
     },
     {
         method: 'DELETE',
-        path: '/users/ID',
+        path: builRoutePath('/users/:id'),
         handler: (req, res) => {
             return res.end()
         },
